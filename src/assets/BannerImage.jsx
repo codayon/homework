fcf5a09ca9f256b73dd95322cf21319ab81bafd3
codayon <reshadahammedayon@proton.me>
2025-07-07 01:42:49 +0600

    fix: Banner image source

--- src/assets/BannerImage.jsx
+++ src/assets/BannerImage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import BannerSrc from './banner.png'
 
 const BannerImage = () => {
   return (
     <div className="w-full overflow-hidden">
       <Link to={'/shop'}>
         <img
-          src="/src/assets/banner.png"
+          src={BannerSrc}
           alt="Website Banner"
           className="w-full h-auto object-cover"
         />
