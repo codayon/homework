5be3330048f923cc5a8a42029d07fbe6e7e8ae81
codayon <reshadahammedayon@proton.me>
2025-07-07 14:57:34 +0600

    Add ads section

---
+++ src/components/layouts/Ads.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Container from '../Container';
+import Flex from '../Flex';
+import AdImgOne from '../../assets/ad-1.png';
+import AdImgTwo from '../../assets/ad-2.png';
+import AdImgThree from '../../assets/ad-3.png';
+import { Link } from 'react-router-dom';
+
+const Ads = () => {
+  return (
+    <section className="pt-44 pb-16">
+      <Container>
+        <Flex className={'gap-x-10'}>
+          <div>
+            <Link to={'/shop'}>
+              <img src={AdImgOne} alt="AdImgOne" />
+            </Link>
+          </div>
+          <Flex className={'flex-col gap-y-10'}>
+            <div>
+              <Link to={'/shop'}>
+                <img src={AdImgTwo} alt="AdImgTwo" />
+              </Link>
+            </div>
+            <div>
+              <Link to={'/shop'}>
+                <img src={AdImgThree} alt="AdImgThree" />
+              </Link>
+            </div>
+          </Flex>
+        </Flex>
+      </Container>
+    </section>
+  );
+};
+
+export default Ads;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BannerImage from '../../assets/BannerImage';
 import Information from '../layouts/Information';
+import Ads from '../layouts/Ads';
 
 const Home = () => {
   return (
     <>
       <BannerImage />
       <Information />
+      <Ads />
     </>
   );
 };
